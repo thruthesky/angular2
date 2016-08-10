741f9122369e4c9f4f9d4bb6a0f80fc0d0b54cbc
jeraisy21 <thruthesky@gmail.com>
2016-08-11 00:21:19 +0900

    Refactornig structure of basic angular 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-//import { NgifComponent } from './ngif.component'
-import {NgSwitchComponent} from './ngSwitch.component';
 @Component({
   selector: 'my-app',
-  template: '<h1>Hello, world!</h1><holder></holder>',
-  //directives: [ NgifComponent ]
-  directives: [ NgSwitchComponent ]
+  template: `
+  Hello, World!
+  `
 })
-export class AppComponent {}
-
-
+export class AppComponent { }
---
+++ app/routing-test/about.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+@Component({
+    selector: 'about',
+    template: `
+    This is about....
+    `
+})
+export class AboutComponent {}
